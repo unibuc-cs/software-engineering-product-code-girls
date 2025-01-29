e235553768dfadd47c07366d9edc56f97240f246
dariahorga <dariahorga@yahoo.com>
2025-01-29 22:58:03 +0200

    Baza de date finala

--- backend/routes/profile-picture.js
+++ backend/routes/profile-picture.js
@@ -2,7 +2,6 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import { db } from '../.config.js';
-import Database from 'better-sqlite3';
 import { verifyToken } from './authentification.js';
 
 const router = express.Router();
